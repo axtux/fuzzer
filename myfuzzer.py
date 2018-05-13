17e1808f65ca225395b9ce961d2041a7b8018ec6
Axtux <axtux@hotmail.com>
2018-05-13 16:26:59 +0200

    run waits to finish, copy input, use crash_file var, print output

--- myfuzzer.py
+++ myfuzzer.py
@@ -8,22 +8,22 @@ import subprocess
 def run(input_file):
   # static version is faster
   args = ['./converter_static', input_file, '/dev/null']
-  # disable closing descriptor/restore signals for performance
-  return subprocess.Popen(args, stderr=subprocess.PIPE, bufsize=4096, close_fds=False, restore_signals=False)
+  return subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 def file_read(filename):
   with open(filename, 'rb') as file:
-    return bytearray(file.read())
+    return file.read()
 
 def file_write(filename, content):
   with open(filename, 'wb') as file:
     file.write(content)
 
-def fuzz_bytes(input, to_randomize):
-  bytes_to_change = random.sample(range(len(input)), to_randomize)
+def fuzz_bytes(input_seed, to_randomize):
+  bytes_array = bytearray(input_seed)
+  bytes_to_change = random.sample(range(len(bytes_array)), to_randomize)
   for b in bytes_to_change:
-    input[b] = random.randint(0, 255)
-  return input
+    bytes_array[b] = random.randint(0, 255)
+  return bytes_array
 
 def fuzz_check(input_seed, max_tests, to_randomize):
   
@@ -42,7 +42,7 @@ def fuzz_check(input_seed, max_tests, to_randomize):
   try:
     to_randomize = round(l*float(to_randomize))
     if to_randomize < 0 or to_randomize > l:
-      usage('PART_TO_RANDOMIZE should be between included 0 and included 1')
+      usage('PART_TO_RANDOMIZE should be between 0 and 1')
   except ValueError:
     usage('"'+to_randomize+'" is not a valid float')
   print('Randomizing', to_randomize, 'bytes\n')
@@ -50,9 +50,10 @@ def fuzz_check(input_seed, max_tests, to_randomize):
   fuzz(input_seed, max_tests, to_randomize)
 
 def fuzz(input_seed, max_tests, to_randomize):
+  crash_file = 'crash/input_0.img'
+  input_file = 'input_0.img'
   for i in range(1, max_tests+1):
     fuzz_input = fuzz_bytes(input_seed, to_randomize)
-    input_file = 'input_0.img'
     file_write(input_file, fuzz_input)
 
     result = run(input_file)
@@ -61,10 +62,10 @@ def fuzz(input_seed, max_tests, to_randomize):
     error = out.find('*') != -1
     
     if error:
-      while os.path.isfile('crash/'+input_file):
-        input_file = 'input_'+str(random.randint(0, 1e6))+'.img'
-      os.rename(input_file, 'crash/'+input_file)
-      print('test', i, 'crashing, saved in', input_file)
+      while os.path.isfile(crash_file):
+        crash_file = 'crash/input_'+str(random.randint(0, 1e6))+'.img'
+      os.rename(input_file, crash_file)
+      print('test', i, 'crashing, saved in', crash_file, ', out', out)
 
 def usage(error=None):
   if error is not None:
