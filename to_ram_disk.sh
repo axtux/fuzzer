32185458bc998d10abeb799bd22807479a32900e
Axtux <axtux@hotmail.com>
2018-05-13 16:29:40 +0200

    ram disk for faster tests

---
+++ to_ram_disk.sh
@@ -0,0 +1,13 @@
+FILES=('myfuzzer.py' 'compare.sh' 'converter_static' 'testinput.img')
+TARGET='/run/user/1000/fuzzer'
+
+#rm -rf "$TARGET"
+mkdir -p "$TARGET/crash"
+
+for FILE in "${FILES[@]}"
+do
+  BASENAME=$(basename "$FILE")
+  cp "$FILE" "$TARGET/$BASENAME"
+done
+
+cd "$TARGET"
